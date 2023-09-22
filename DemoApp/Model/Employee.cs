176b8f47077008352f66a0980692f129d46cdb84
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2023-09-22 13:48:37 +0200

    add demo app

---
+++ DemoApp/Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Employee
+    {
+        public int Id { get; set; }  
+        public string Password { get; set; }
+        public EmployeeType Type { get; set; }
+        public string Location { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Ticket { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+    public enum EmployeeType { Regular, ServiceDesk}
+}
---
+++ DemoApp/Model/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Ticket
+    {
+        public Status TicketStatus;
+        public IncidentType Type { get; set; }
+        public string IncidentSubject { get; set; }
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public Priority PriorityType { get; set; }
+        public string Deadline { get; set; }
+        public string Description { get; set; }
+
+        public enum Status
+        {
+            Open, Resolved, Close
+        }
+        public enum IncidentType { }
+
+        public enum Priority { }
+
+    }
+}

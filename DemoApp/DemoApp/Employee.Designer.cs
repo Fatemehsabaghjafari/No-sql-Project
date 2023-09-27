142be5dbbf641bb1c4ce3a49827e6b12d66c1754
tanvirshihab <699290@student.inholland.nl>
2023-09-27 11:54:13 +0200

    added design

---
+++ DemoApp/DemoApp/Employee.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace DemoApp
+{
+    partial class Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create New User";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First name:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last name:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type of user:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "E-mai address:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(16, 481);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(184, 37);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Phone number:";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(16, 542);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(229, 37);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Location brunch:";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(18, 598);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(214, 37);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Send password?";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(332, 598);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(450, 37);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Yes, a password e-mail will be sent to the user";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(294, 596);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(35, 37);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(294, 252);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 30);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(294, 307);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(454, 30);
+            this.textBox2.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(294, 363);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(454, 28);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Employee";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(294, 421);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(454, 30);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(294, 481);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(454, 30);
+            this.textBox4.TabIndex = 15;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(294, 542);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(454, 28);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.Text = "Haarlem";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(558, 681);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 57);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "ADD USER";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(294, 681);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 57);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "CANCEL";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 774);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Employee";
+            this.Text = "Haarlem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
--- DemoApp/DemoApp/Form1.Designer.cs
+++ DemoApp/DemoApp/Form1.Designer.cs
@@ -29,11 +29,11 @@ namespace DemoApp
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("");
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -41,13 +41,15 @@ namespace DemoApp
             this.button3 = new System.Windows.Forms.Button();
             this.DashboardPanel = new System.Windows.Forms.Panel();
             this.IncidentViewPanel = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TicketslistView = new System.Windows.Forms.ListView();
+            this.AddIncidentBtn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.UserViewPanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.AddIncidentBtn = new System.Windows.Forms.Button();
-            this.TicketslistView = new System.Windows.Forms.ListView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.DashboardPanel.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.IncidentViewPanel.SuspendLayout();
             this.UserViewPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -100,8 +102,7 @@ namespace DemoApp
             // 
             // DashboardPanel
             // 
-            this.DashboardPanel.Controls.Add(this.IncidentViewPanel);
-            this.DashboardPanel.Location = new System.Drawing.Point(3, 3);
+            this.DashboardPanel.Location = new System.Drawing.Point(0, 0);
             this.DashboardPanel.Name = "DashboardPanel";
             this.DashboardPanel.Size = new System.Drawing.Size(1006, 582);
             this.DashboardPanel.TabIndex = 6;
@@ -112,35 +113,33 @@ namespace DemoApp
             this.IncidentViewPanel.Controls.Add(this.TicketslistView);
             this.IncidentViewPanel.Controls.Add(this.AddIncidentBtn);
             this.IncidentViewPanel.Controls.Add(this.label3);
-            this.IncidentViewPanel.Location = new System.Drawing.Point(3, 0);
+            this.IncidentViewPanel.Location = new System.Drawing.Point(0, 0);
             this.IncidentViewPanel.Name = "IncidentViewPanel";
             this.IncidentViewPanel.Size = new System.Drawing.Size(1006, 582);
             this.IncidentViewPanel.TabIndex = 7;
             // 
-            // label3
+            // textBox1
             // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(26, 12);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(225, 39);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Overview tickets";
+            this.textBox1.Location = new System.Drawing.Point(31, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // UserViewPanel
+            // TicketslistView
             // 
-            this.UserViewPanel.Controls.Add(this.DashboardPanel);
-            this.UserViewPanel.Location = new System.Drawing.Point(12, 145);
-            this.UserViewPanel.Name = "UserViewPanel";
-            this.UserViewPanel.Size = new System.Drawing.Size(1006, 582);
-            this.UserViewPanel.TabIndex = 8;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.TicketslistView.HideSelection = false;
+            this.TicketslistView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5});
+            this.TicketslistView.Location = new System.Drawing.Point(31, 131);
+            this.TicketslistView.Name = "TicketslistView";
+            this.TicketslistView.Size = new System.Drawing.Size(910, 434);
+            this.TicketslistView.TabIndex = 4;
+            this.TicketslistView.UseCompatibleStateImageBehavior = false;
             // 
             // AddIncidentBtn
             // 
@@ -154,36 +153,79 @@ namespace DemoApp
             this.AddIncidentBtn.UseVisualStyleBackColor = false;
             this.AddIncidentBtn.Click += new System.EventHandler(this.AddIncidentBtn_Click);
             // 
-            // TicketslistView
+            // label3
             // 
-            this.TicketslistView.HideSelection = false;
-            this.TicketslistView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem6,
-            listViewItem7,
-            listViewItem8,
-            listViewItem9,
-            listViewItem10});
-            this.TicketslistView.Location = new System.Drawing.Point(31, 131);
-            this.TicketslistView.Name = "TicketslistView";
-            this.TicketslistView.Size = new System.Drawing.Size(910, 434);
-            this.TicketslistView.TabIndex = 4;
-            this.TicketslistView.UseCompatibleStateImageBehavior = false;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Overview tickets";
             // 
-            // textBox1
+            // UserViewPanel
             // 
-            this.textBox1.Location = new System.Drawing.Point(31, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(184, 22);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.UserViewPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.UserViewPanel.Controls.Add(this.button4);
+            this.UserViewPanel.Controls.Add(this.textBox2);
+            this.UserViewPanel.Controls.Add(this.label4);
+            this.UserViewPanel.Location = new System.Drawing.Point(12, 145);
+            this.UserViewPanel.Name = "UserViewPanel";
+            this.UserViewPanel.Size = new System.Drawing.Size(1006, 805);
+            this.UserViewPanel.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(369, 51);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "User management";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox2.Location = new System.Drawing.Point(28, 72);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(338, 49);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Filter by E-mail";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.GreenYellow;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button4.Location = new System.Drawing.Point(484, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 57);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "+ ADD NEW USER";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 789);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1066, 996);
             this.Controls.Add(this.UserViewPanel);
+            this.Controls.Add(this.DashboardPanel);
+            this.Controls.Add(this.IncidentViewPanel);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -193,10 +235,10 @@ namespace DemoApp
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.DashboardPanel.ResumeLayout(false);
             this.IncidentViewPanel.ResumeLayout(false);
             this.IncidentViewPanel.PerformLayout();
             this.UserViewPanel.ResumeLayout(false);
+            this.UserViewPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -217,6 +259,9 @@ namespace DemoApp
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListView TicketslistView;
         private System.Windows.Forms.Button AddIncidentBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
 

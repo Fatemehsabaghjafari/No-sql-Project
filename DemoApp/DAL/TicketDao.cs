242f06f8aca70f99282455d6841ec2e0660e9aac
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2023-11-07 07:07:27 +0100

    dao's method for ticket was removed mistakenly

--- DemoApp/DAL/TicketDao.cs
+++ DemoApp/DAL/TicketDao.cs
@@ -16,13 +16,20 @@ namespace DAL
             collection1 = database.GetCollection<Ticket>("Garden-group"); // Specify your ticket collection name here
           
         }
-       
-       
 
-        
+        public List<Ticket> GetAllTickets()
+        {
+            return collection1.Find(_ => true).ToList();
+        }
+        public void AddTicket(Ticket ticket)
+        {
+            collection1.InsertOne(ticket);
+        }
 
 
-        
+
+
+
 
     }
 }
